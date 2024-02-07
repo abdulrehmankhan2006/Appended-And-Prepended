198ec1560f47850bc241778d702e5d63a2aef290
Abdul Rehman Khan <145275109+abdulrehmankhan2006@users.noreply.github.com>
2024-02-07 14:11:09 +0500

    Add files via upload

---
+++ Javascriptvariable.js
@@ -0,0 +1,16 @@
+// Q1 Multiply Number
+let num1 = 5;
+let num2 = 3;
+let result = num1 * num2;
+console.log(result);
+
+// Q2 Divide Number
+let divide = 10 / 2;
+console.log(divide);
+
+// Q3 Addition ANd Multipliction Number
+let a = 4;
+let b = 5;
+let c = 6;
+let sum = a = b * c;
+console.log(sum);
